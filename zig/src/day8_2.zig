bdbd3a9029aebce0fa19999e1f978332571b4123
Nate Young <nathaniel.young@mpls.frb.org>
2025-12-09 23:40:10 -0600

    part 2 solved

--- zig/src/day8_2.zig
+++ zig/src/day8_2.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Reader = std.io.Reader;
 
+
 const Point = struct {
     x: i64,
     y: i64,
@@ -19,30 +20,6 @@ const Point = struct {
         const sum: f32 = @floatFromInt(x2 + y2 + z2);
         return std.math.sqrt(sum);
     }
-
-    pub fn dist_d(self: Point, other: Point, dim: u8) f32 {
-        const dx = other.x - self.x;
-        const x2 = dx * dx;
-        const dy = other.y - self.y;
-        const y2 = dy * dy;
-        const dz = other.z - self.z;
-        const z2 = dz * dz;
-
-        switch (dim) {
-            1 => {
-                const result: f32 = @floatFromInt(dx);
-                return result;
-            },
-            2 => {
-                const sum: f32 = @floatFromInt(x2 + y2);
-                return std.math.sqrt(sum);
-            },
-            else => {
-                const sum: f32 = @floatFromInt(x2 + y2 + z2);
-                return std.math.sqrt(sum);
-            },
-        }
-    }
 };
 
 pub fn main() !void {
@@ -53,8 +30,8 @@ pub fn main() !void {
 
     const argv = try std.process.argsAlloc(allocator);
 
-    if (argv.len < 3) {
-        std.process.fatal("Expected input and num_circuits argument, but only {d} were given\n", .{ argv.len - 1 });
+    if (argv.len < 2) {
+        std.process.fatal("Expected input argument, but none given\n", .{});
     }
 
     const infile = try std.fs.cwd().openFile(argv[1], .{ .mode = .read_only });
@@ -63,48 +40,56 @@ pub fn main() !void {
     var infile_reader = infile.reader(try allocator.alloc(u8, 20 * 1024));
     const input = &infile_reader.interface;
 
-    const num_circuits: u32 = try std.fmt.parseInt(u32, argv[2], 10);
-
     var i: usize = 0;
-    var points: [1024]Point = undefined;
+    var points_arr: [1024]Point = undefined;
     while (input.peekByte()) |_| {
-        points[i] = try readPoint(input);
+        points_arr[i] = try readPoint(input);
         i += 1;
     } else |_| {
         // end of input
     }
-    std.debug.print("Read {d} points\n", .{ i });
+    const points = points_arr[0..i];
+    std.debug.print("Read {d} points\n", .{ points.len });
 
     var next_circuit: u32 = 1;
     var counts_arr = [_]u32{0} ** 1024;
     var min_dist: f32 = undefined;
-    for (0..num_circuits) |n| {
-        if(closest_pair(points[0..i], &min_dist)) |idxs| {
+    var max_count: u32 = 0;
+    var last_pair: [2]*Point = undefined;
+    while (max_count < points.len) {
+        if (closest_pair(points, &min_dist)) |idxs| {
             var p = &points[idxs[0]];
             var q = &points[idxs[1]];
-            const d = p.dist(q.*);
-            std.debug.print("{d}: Closest points: {d},{d},{d} and {d},{d},{d} at dist {d}\n", .{ n, p.x, p.y, p.z, q.x, q.y, q.z, d });
-            std.debug.print("Circuit {d} ({d}), {d} ({d})\n", .{ p.circuit, counts_arr[p.circuit], q.circuit, counts_arr[q.circuit] });
 
             if (p.circuit == 0 and q.circuit == 0) {
-                std.debug.print("Placing {d},{d},{d} and {d},{d},{d} on their own circuit {d}\n", .{ p.x, p.y, p.z, q.x, q.y, q.z, next_circuit });
                 p.circuit = next_circuit;
                 q.circuit = next_circuit;
                 counts_arr[next_circuit] += 2;
+                if (counts_arr[next_circuit] > max_count) {
+                    max_count = counts_arr[next_circuit];
+                    last_pair[0] = p;
+                    last_pair[1] = q;
+                }
                 next_circuit += 1;
             } else if (p.circuit == q.circuit) {
-                std.debug.print("Both already connected\n", .{});
-            } else if (q.circuit == 0) {
-                std.debug.print("Adding {d},{d},{d} to circuit {d}\n", .{ q.x, q.y, q.z, p.circuit });
-                q.circuit = p.circuit;
-                counts_arr[q.circuit] += 1;
+                // already connected
             } else if (p.circuit == 0) {
-                std.debug.print("Adding {d},{d},{d} to circuit {d}\n", .{ p.x, p.y, p.z, q.circuit });
                 p.circuit = q.circuit;
                 counts_arr[p.circuit] += 1;
+                if (counts_arr[p.circuit] > max_count) {
+                    max_count = counts_arr[p.circuit];
+                    last_pair[0] = p;
+                    last_pair[1] = q;
+                }
+            } else if (q.circuit == 0) {
+                q.circuit = p.circuit;
+                counts_arr[q.circuit] += 1;
+                if (counts_arr[q.circuit] > max_count) {
+                    max_count = counts_arr[q.circuit];
+                    last_pair[0] = p;
+                    last_pair[1] = q;
+                }
             } else {
-                // add all boxes in q's circuit to p's circuit
-                std.debug.print("Combining circuit {d} with circuit {d}\n", .{ p.circuit, q.circuit });
                 counts_arr[p.circuit] += counts_arr[q.circuit];
                 counts_arr[q.circuit] = 0;
                 const qc = q.circuit;
@@ -114,24 +99,17 @@ pub fn main() !void {
                         x.circuit = p.circuit;
                     }
                 }
+                if (counts_arr[p.circuit] > max_count) {
+                    max_count = counts_arr[p.circuit];
+                    last_pair[0] = p;
+                    last_pair[1] = q;
+                }
             }
-
-            std.debug.print("Circuit {d} ({d}), {d} ({d})\n", .{ p.circuit, counts_arr[p.circuit], q.circuit, counts_arr[q.circuit] });
-            std.debug.print("\n", .{});
         }
+        std.debug.print("Largest circuit contains: {d}\n", .{ max_count });
     }
 
-    const counts = counts_arr[1..next_circuit];
-    std.mem.sort(u32, counts, {}, std.sort.desc(u32));
-    for(counts) |c| {
-        std.debug.print("{d} ", .{ c });
-    }
-    std.debug.print("\n", .{});
-
-    var answer: u32 = 1;
-    for (counts[0..3]) |c| {
-        answer *= c;
-    }
+    const answer = last_pair[0].x * last_pair[1].x;
     std.debug.print("Answer: {d}\n", .{ answer });
 }
 
